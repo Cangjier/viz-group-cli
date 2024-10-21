68545cb1bd63431d95a73c1118c11e6f98195980
OpenPlugin <14420465+openplugin@user.noreply.gitee.com>
2024-10-21 11:36:41 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using TidyHPC.Loggers;
+using TidyHPC.Routers;
+using TidyHPC.Routers.Args;
+using VizGroup;
+using VizGroup.V1;
+Logger.FilePath = Path.Combine(Util.CreateDirectory(Path.Combine(Util.GetDataDirectory(),"Logs")),$"{Path.GetFileName(Environment.ProcessPath)}-{DateTime.Now.ToString("yyyy.MM.dd-HH.mm.ss")}.log");
+Logger.QueueLogger.OnLine += async (message) =>
+{
+    Console.WriteLine(message);
+    await Task.CompletedTask;
+};
+
+ArgsRouter argsRouter = new();
+
+async Task startServer(
+    [ArgsAliases("--port")] string port,
+    [ArgsAliases("--cert")] string certPath = "",
+    [ArgsAliases("--cert-password")] string certPassword = "")
+{
+    Application application = new();
+    try
+    {
+        await application.Start(new()
+        {
+            ServerPorts = [int.Parse(port)],
+            SSLCertificatePath = certPath,
+            SSLCertificatePassword = certPassword,
+            EnablePlugins = false
+        });
+    }
+    catch (Exception e)
+    {
+        Logger.Error(e);
+        throw;
+    }
+}
+
+argsRouter.Register(["server"], startServer);
+
+async Task client(
+    [ArgsAliases("--port")] string port,
+    [ArgsAliases("--share-server-url-prefix")] string shareServerUrlPrefix,
+    [ArgsAliases("--database-path")] string? databasePath = null,
+    [ArgsAliases("--plugins-directory")] string? pluginsDirectory = null)
+{
+    Application application = new();
+    try
+    {
+        await application.Start(new()
+        {
+            ServerPorts = [int.Parse(port)],
+            EnablePlugins = true,
+            EnableShareServer = true,
+            ShareServerUrlPrefix = shareServerUrlPrefix,
+            DatabasePath = databasePath,
+            PluginsDirectory = pluginsDirectory
+        });
+    }
+    catch (Exception e)
+    {
+        Logger.Error(e);
+        throw;
+    }
+}
+
+argsRouter.Register(["client"], client);
+
+await argsRouter.Route(args);
---
+++ Readme.md
@@ -0,0 +1,10 @@
+# 使用教程
+## Server
+``` bash
+VizGroup.CLI server --port 8080 --cert ssl.pem --cert-password 123456
+```
+
+## Client
+``` bash
+VizGroup.CLI client --port 8080 --share-server-url-prefix "http://192.168.0.111:8080/" --database-path Agent/Agent.db --plugins-directory Agent/Plugins
+```
